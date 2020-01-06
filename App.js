fa6baac97710781f03014995ff4177160f471809
JillGlass <glass.nz@gmail.com>
2020-01-06 15:47:10 +1300

    moved styles to stylesheet

--- App.js
+++ App.js
@@ -3,16 +3,29 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput
-          placeholder="Enter Job"
-          style={{ width: '80%', borderColor: "black", borderWidth: 1, padding: 10 }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter Job" style={styles.input} />
         <Button title="ADD" />
       </View>
+      <View />
     </View>
   );
 }
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  }
+});
