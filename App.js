434cea974ce7e67c1d8f304fa35bb64716c2a957
JillGlass <glass.nz@gmail.com>
2020-01-06 14:44:02 +1300

    add ADD button

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 30}}>
+      <View>
+    <TextInput placeholder='Enter Job' style={{borderBottomColor: 'black'}}/>
+    <Button title='ADD'/>
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
