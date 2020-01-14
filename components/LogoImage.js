c0a552aa23efc7d128dfd94340102402900fa26a
JillGlass <glass.nz@gmail.com>
2020-01-14 15:54:52 +1300

    image added need to resize

--- components/LogoImage.js
+++ components/LogoImage.js
@@ -3,16 +3,15 @@ import { View, StyleSheet, Image, ImageBackground } from "react-native";
 
 const LogoImage = () => {
   return (
-    <View style={styles.logo}>
-    <Image source={require("../assets/images/undraw_welcome_3gvl.svg")}/>
+    <View style={styles.icon}>
+    <Image source={require("../assets/trust.png")}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  logo: {
-    position: 'absolute',
-    height: 100
+  icon: {
+    width: 50,
   }
 });
 
