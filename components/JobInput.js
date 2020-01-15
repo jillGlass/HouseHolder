0991c5f26398fe0b296726da67861a0fef9f3673
JillGlass <glass.nz@gmail.com>
2020-01-15 21:05:47 +1300

    allocation modal complete, logic half done

--- components/JobInput.js
+++ components/JobInput.js
@@ -7,25 +7,27 @@ import {
   Modal,
   Slider
 } from "react-native";
+import Allocation from "./Allocation";
 
 const JobInput = props => {
   const [enteredJob, setEnteredJob] = useState("");
+  const [isNameMode, setIsNameMode] = useState(false);
+  
+
+
   const jobInputHandler = enteredText => {
     setEnteredJob(enteredText);
   };
 
-  const addJobHandler = () => {
-    props.onAddJob(enteredJob);
-    setEnteredJob("");
-  };
+  
+// move below to Allocation modal? Or keep here and place in a larger function?
+  // const addJobHandler = () => {
+  //   props.onAddJob(enteredJob);
+  //   setEnteredJob("");
+  // };
 
-  const openModal=() => {
-    this.setState({ modalVisible: true });
-  }
 
-  const closeModal=() => {
-    this.setState({ modalVisible: false });
-  }
+  
 
   return (
     <Modal visible={props.visible} animationType="slide">
@@ -41,8 +43,11 @@ const JobInput = props => {
             <Button color="red" title="CANCEL" onPress={props.onCancel} />
           </View>
           <View style={styles.button}>
-            <Button style={styles.addBtn} title="ADD" onPress={addJobHandler} />
+            <Button style={styles.addBtn} title="ADD" onPress={setIsNameMode} />
           </View>
+          <Allocation
+          // visible={SelectedName}
+          />
         </View>
       </View>
     </Modal>
